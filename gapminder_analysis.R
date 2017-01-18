b67b616ac0343bc0a674b6725fddc98ca78d28a3
Olivia L. McGovern <livmcgov@umich.edu>
2017-01-18 11:14:00 -0500

    Added Author

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,3 +1,7 @@
+#  Notes from Software Carpentry Worshop
+#  January 17, 2017
+#  Author: Olivia McGovern 
+
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 head(gapminder)
